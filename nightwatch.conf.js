9230439ba879fd522ce2fdf19b9c5a9db1e33380
Shital <devshital17@gmail.com>
2020-09-07 11:43:23 +0530

    done

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -3,14 +3,14 @@ module.exports = {
   
     "webdriver" : {
       "start_process": true,
-      "server_path": "./node_modules/.bin/chromedriver",
+      "server_path": "node_modules/chromedriver/lib/chromedriver/chromedriver.exe",
       "port": 9515
     },
   
     "test_settings" : {
       "default" : {
         "desiredCapabilities": {
-          "browserName": "chromedriver"
+          "browserName": "chrome"
         }
       }
     }
